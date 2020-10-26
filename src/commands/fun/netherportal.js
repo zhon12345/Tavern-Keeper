d823c8972cf383e63b88d8cfef1ecc7c24695c2f
zhon12345 <43035773+zhon12345@users.noreply.github.com>
2020-10-26 21:17:27 +0800

    Added Stuff

---
+++ src/commands/fun/netherportal.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+module.exports = {
+	name: 'portal',
+	category: 'Fun',
+	description: 'Surely you know what lenny is, everyone does.',
+	aliases: ['nether'],
+	usage: 'portal  ',
+	run: async (client, message, args) => {
+		message.channel.send('<:obsidian:769399257165463592><:obsidian:769399257165463592><:obsidian:769399257165463592><:obsidian:769399257165463592>\n<:obsidian:769399257165463592><a:portal:769399213724532746><a:portal:769399213724532746><:obsidian:769399257165463592>\n<:obsidian:769399257165463592><a:portal:769399213724532746><a:portal:769399213724532746><:obsidian:769399257165463592>\n<:obsidian:769399257165463592><a:portal:769399213724532746><a:portal:769399213724532746><:obsidian:769399257165463592>\n<:obsidian:769399257165463592><:obsidian:769399257165463592><:obsidian:769399257165463592><:obsidian:769399257165463592>');
+	},
+};
