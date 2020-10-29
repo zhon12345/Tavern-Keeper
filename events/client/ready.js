88c051c9ee85ec0263848d815899c8edad334ddd
zhon12345 <43035773+zhon12345@users.noreply.github.com>
2020-10-29 11:49:15 +0800

    Updated Stuff

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,11 +1,17 @@
-const { BOT_PREFIX } = process.env;
+const { BOT_PREFIX, TOPGG_TOKEN } = process.env;
+const DBL = require("dblapi.js");
 
 module.exports = async (client) => {
+	const dbl = new DBL(TOPGG_TOKEN, client);
 	const botStatus = [
 		`${client.commands.size} Commands`,
 		`${client.users.cache.size} Users`,
 		`${client.guilds.cache.size} Servers`,
 	];
+
+	setInterval(() => {
+		dbl.postStats(client.guilds.cache.size);
+	}, 1800000);
 
 	setInterval(function() {
 		const status = botStatus[Math.floor(Math.random() * botStatus.length)];
