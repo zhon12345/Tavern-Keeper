0e7492bc6fc0ca39ebd4f578ab2c025b1ecee35b
zhon12345 <43035773+zhon12345@users.noreply.github.com>
2020-11-15 17:05:36 +0000

    🔥 Removed temp code in index.js

--- commands/info/links.js
+++ commands/info/links.js
@@ -16,7 +16,7 @@ module.exports = {
 			.setColor("BLUE")
 			.addFields(
 				{ name: "Invite link", value: "[Invite](https://discord.com/oauth2/authorize?client_id=722414315595890699&scope=bot&permissions=805314622)", inline: true },
-				{ name: "Website", value: "[Website](https://tavernkeeper.ml/)", inline: true },
+				{ name: "Website", value: "[Website](http://tavernkeeper.ml/)", inline: true },
 				{ name: "Official Discord Server", value: "[Discord Server](https://discord.gg/jMpw3jw)", inline: true },
 				{ name: "Discord Boats", value: "[Vote](https://discord.boats/bot/722054700308103200)", inline:true },
 				{ name: "Discord Bots", value: "[Vote](https://discord.bots.gg/bots/722054700308103200)", inline:true },
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 require("dotenv").config();
 const keepAlive = require("./server");
-const Guild = require("./models/guild");
-const mongoose = require("mongoose");
 const { Client, Collection } = require("discord.js");
 const client = new Client({ disableMentions: "everyone", ws: { intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS", "GUILD_PRESENCES"] } });
 
@@ -12,32 +10,6 @@ client.snipes = new Collection();
 
 ["command", "event"].forEach(handler => {
 	require(`./handlers/${handler}`)(client);
-});
-
-client.on("message", async message => {
-	if(message.content === ">update" && message.author.id === process.env.BOT_OWNER) {
-		client.guilds.forEach(guild => {
-			guild = new Guild({
-				_id: mongoose.Types.ObjectId(),
-				guildID: guild.id,
-				guildName: guild.name,
-				prefix: process.env.BOT_PREFIX,
-				blacklisted: false,
-				settings:{
-					antiprofanity: false,
-					antilinks: false,
-					muterole: null,
-					memberrole: null,
-					modlog: null,
-					serverlog: null,
-					messagelog: null,
-				},
-			});
-
-			guild.save();
-			message.channel.send(`updating ${guild.name}`);
-		});
-	}
 });
 
 keepAlive();
