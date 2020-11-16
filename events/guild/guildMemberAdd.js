10ae0b0c880e69a3b5dd520e9474cfeab59e6d26
Ng Zhun Onn <ngzhunonn@gmail.com>
2020-11-16 22:27:22 +0800

    🔊 Added server and message logs logging

---
+++ events/guild/guildMemberAdd.js
@@ -0,0 +1,15 @@
+const Guild = require("../../models/guild");
+const moment = require("moment");
+
+module.exports = async (client, member) => {
+	const settings = await Guild.findOne({
+		guildID: member.guild.id,
+	});
+
+	const logchannel = member.guild.channels.cache.get(settings.settings.serverlog);
+	if (!logchannel) return;
+
+	logchannel.send(
+		`\`[${moment(member.joinedTimestamp).format("HH:mm:ss")}]\` 📥 **${member.user.username}**#${member.user.discriminator} (ID: ${member.user.id}) joined the server.\n\`[Creation Date]\` ${moment(member.user.createdTimestamp).format("dddd, Do MMMM YYYY, h:mm:ss a")}`,
+	);
+};
---
+++ events/guild/messageDeleteBulk.js
@@ -0,0 +1,41 @@
+const moment = require("moment");
+const fetch = require("node-fetch");
+const url = "https://hasteb.in/documents";
+const Guild = require("../../models/guild");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = async (client, messages) =>{
+	const guildArray = [];
+
+	messages.forEach(async (mes) =>{
+		if(mes.partial) await mes.fetch();
+		guildArray.push(mes.guild);
+	});
+
+	const guild = guildArray[0];
+	const settings = await Guild.findOne({
+		guildID: guild.id,
+	});
+
+	const logsChannel = client.channels.cache.get(settings.settings.messagelog);
+	if(!logsChannel) return;
+
+	const output = messages.map(m => `${new Date(m.createdAt).toLocaleString("en-US")} - ${m.author.tag}: ${m.attachments.size > 0 ? m.attachments.first().proxyURL : m.content}`).join("\n");
+
+	let response;
+	try {
+		response = await fetch(url, { method: "POST", body: output, headers: { "Content-Type": "text/plain" } }).then(res => res.json());
+	}
+	catch (e) {
+		return logsChannel.channel.send("<:vError:725270799124004934> An error occured, please try again!");
+	}
+	const embed = new MessageEmbed()
+		.setDescription(`[\`📄 View\`](${output.length > 0 ? `https://hasteb.in/${response.key}.js` : output})`)
+		.setColor("RED");
+
+	await logsChannel.send(
+		`\`[${moment(Date.now()).format("HH:mm:ss")}]\` 🗑️ **${messages.first().author.username}**#${messages.first().author.discriminator} cleared \`${messages.size - 1}\` messages in ${messages.first().channel}`,
+		embed,
+	);
+
+};
