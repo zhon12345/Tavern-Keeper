b886504f3ca9de5da680f0587ea5c0d9a0dd078c
zhon12345 <43035773+zhon12345@users.noreply.github.com>
2020-11-16 22:27:22 +0800

    🔊 Added server and message logs logging

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -1,3 +1,7 @@
+const { MessageEmbed } = require("discord.js");
+const Guild = require("../../models/guild");
+const moment = require("moment");
+
 module.exports = async (client, message) => {
 	if (message.author.bot) return;
 	const snipes = message.client.snipes.get(message.channel.id) || [];
@@ -10,4 +14,39 @@ module.exports = async (client, message) => {
 
 	snipes.splice(10);
 	message.client.snipes.set(message.channel.id, snipes);
+
+	const settings = await Guild.findOne({
+		guildID: message.guild.id,
+	});
+
+	const fetchedLogs = await message.guild.fetchAuditLogs({
+		limit: 1,
+		type: "MESSAGE_DELETE",
+	});
+	const deletionLog = fetchedLogs.entries.first();
+	const { executor } = deletionLog;
+
+	const channel = message.guild.channels.cache.get(settings.settings.messagelog);
+	if (!channel) return;
+
+	if(executor.id !== message.author.id) {
+		const embed = new MessageEmbed()
+			.setColor("RED")
+			.addFields(
+				{ name: "Content:", value:`${message.attachments.size > 0 ? message.attachments.first().proxyURL : message.content >= 1024 ? `${message.content.slice(0, 1021)}...` : message.content}` },
+			);
+		channel.send(
+			`\`[${moment(Date.now()).format("HH:mm:ss")}]\` ❌ **${message.author.username}**#${message.author.discriminator} (ID: ${message.author.id})'s message has been deleted by **${executor.username}**#${executor.discriminator} in ${message.channel}.`, embed,
+		);
+	}
+	else {
+		const embed = new MessageEmbed()
+			.setColor("RED")
+			.addFields(
+				{ name: "Content:", value:`${message.attachments.size > 0 ? message.attachments.first().proxyURL : message.content >= 1024 ? `${message.content.slice(0, 1021)}...` : message.content}` },
+			);
+		channel.send(
+			`\`[${moment(Date.now()).format("HH:mm:ss")}]\` ❌ **${message.author.username}**#${message.author.discriminator} (ID: ${message.author.id}) deleted a message in ${message.channel}.`, embed,
+		);
+	}
 };
---
+++ events/guild/messageUpdate.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-empty */
+const { MessageEmbed } = require("discord.js");
+const moment = require("moment");
+const Guild = require("../../models/guild");
+
+module.exports = async (client, oldMessage, newMessage) => {
+	const settings = await Guild.findOne({
+		guildID: newMessage.guild.id,
+	});
+
+	if(oldMessage.author.bot) return;
+	if(oldMessage === newMessage) return;
+
+	const embed = new MessageEmbed()
+		.setColor("YELLOW")
+		.addFields(
+			{ name: "Before", value: oldMessage.attachments.size > 0 ? oldMessage.attachments.first().proxyURL : oldMessage.content >= 1024 ? `${oldMessage.content.slice(0, 1021)}...` : oldMessage.content, inline: true },
+			{ name: "After", value: newMessage.attachments.size > 0 ? newMessage.attachments.first().proxyURL : newMessage.content >= 1024 ? `${newMessage.content.slice(0, 1021)}...` : newMessage.content, inline: true },
+		);
+
+	const channel = oldMessage.guild.channels.cache.get(settings.settings.messagelog);
+	if (!channel) return;
+
+	channel.send(
+		`\`[${moment(Date.now()).format("HH:mm:ss")}]\` ✏️ **${oldMessage.author.username}**#${oldMessage.author.discriminator} (ID: ${oldMessage.author.id}) edited a message in ${oldMessage.channel}.`, embed,
+	);
+};
