5e724741e81eb2baf5f9927c3bd6717cdc461d24
zhon12345 <43035773+zhon12345@users.noreply.github.com>
2020-12-14 21:23:35 +0800

    📦 Switched hosting from repl.it to heroku

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 require("dotenv").config();
-const keepAlive = require("./server");
 const { Client, Collection } = require("discord.js");
 const client = new Client({
-	disableMentions: "everyone",
-	ws: { intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS", "GUILD_PRESENCES", "GUILD_VOICE_STATES"] },
+    disableMentions: "everyone",
+    ws: { intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS", "GUILD_PRESENCES", "GUILD_VOICE_STATES"] },
 });
 
 client.commands = new Collection();
@@ -12,8 +11,7 @@ client.category = new Collection();
 client.snipes = new Collection();
 
 ["command", "event"].forEach(handler => {
-	require(`./handlers/${handler}`)(client);
+    require(`./handlers/${handler}`)(client);
 });
 
-keepAlive();
 client.login(process.env.BOT_TOKEN);
