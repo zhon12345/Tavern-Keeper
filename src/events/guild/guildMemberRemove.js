6206542a3daa305c787b8ce86f09aaedddd95225
zhon12345 <43035773+zhon12345@users.noreply.github.com>
2021-01-14 03:15:14 +0800

    🐛 Fixed Errors

--- src/events/guild/guildMemberRemove.js
+++ src/events/guild/guildMemberRemove.js
@@ -2,35 +2,14 @@ const Guild = require('../../models/guild');
 const moment = require('moment');
 
 module.exports = async (client, member) => {
-	if(!member.guild.me.hasPermission('VIEW_AUDIT_LOG')) return;
 	const settings = await Guild.findOne({
 		guildID: member.guild.id,
-	});
-
-	const isKicked = await member.guild.fetchAuditLogs({
-		type: 'MEMBER_KICK',
-	});
-
-	const isBanned = await member.guild.fetchAuditLogs({
-		type: 'MEMBER_BAN',
 	});
 
 	const logchannel = member.guild.channels.cache.get(settings.settings.serverlog);
 	if (!logchannel) return;
 
-	if(isKicked.entries.first().target.id === member.id) {
-		logchannel.send(
-			`\`[${moment(Date.now()).format('HH:mm:ss')}]\` 📤 **${member.user.username}**#${member.user.discriminator} (ID: ${member.user.id}) was kicked from the server.\n\`[Joined Date]\` ${moment(member.user.joinedAt).format('dddd, Do MMMM YYYY, h:mm:ss a')}`,
-		);
-	}
-	else if(isBanned.entries.first().target.id === member.id) {
-		logchannel.send(
-			`\`[${moment(Date.now()).format('HH:mm:ss')}]\` 📤 **${member.user.username}**#${member.user.discriminator} (ID: ${member.user.id}) was banned from the server.\n\`[Joined Date]\` ${moment(member.user.joinedAt).format('dddd, Do MMMM YYYY, h:mm:ss a')}`,
-		);
-	}
-	else {
-		logchannel.send(
-			`\`[${moment(Date.now()).format('HH:mm:ss')}]\` 📤 **${member.user.username}**#${member.user.discriminator} (ID: ${member.user.id}) left the server.\n\`[Joined Date]\` ${moment(member.user.joinedAt).format('dddd, Do MMMM YYYY, h:mm:ss a')}`,
-		);
-	}
+	logchannel.send(
+		`\`[${moment(Date.now()).format('HH:mm:ss')}]\` 📤 **${member.user.username}**#${member.user.discriminator} (ID: ${member.user.id}) left, was kicked or banned from the server.\n\`[Joined Date]\` ${moment(member.user.joinedAt).format('dddd, Do MMMM YYYY, h:mm:ss a')}`,
+	);
 };
--- src/events/guild/messageDelete.js
+++ src/events/guild/messageDelete.js
@@ -25,20 +25,18 @@ module.exports = async (client, message) => {
 		limit: 1,
 		type: 'MESSAGE_DELETE',
 	});
-	const deletionLog = fetchedLogs.entries.first();
-	const { executor } = deletionLog;
 
 	const channel = message.guild.channels.cache.get(settings.settings.messagelog);
 	if (!channel) return;
 
-	if(executor.id !== message.author.id) {
+	if(fetchedLogs.entries.first().executor.id !== message.author.id) {
 		const embed = new MessageEmbed()
 			.setColor('RED')
 			.addFields(
 				{ name: 'Content:', value:`${message.attachments.size > 0 ? message.attachments.first().proxyURL : message.content >= 1024 ? `${message.content.slice(0, 1021)}...` : message.content}` },
 			);
 		channel.send(
-			`\`[${moment(Date.now()).format('HH:mm:ss')}]\` ❌ **${message.author.username}**#${message.author.discriminator} (ID: ${message.author.id})'s message has been deleted by **${executor.username}**#${executor.discriminator} in ${message.channel}.`, embed,
+			`\`[${moment(Date.now()).format('HH:mm:ss')}]\` ❌ **${message.author.username}**#${message.author.discriminator} (ID: ${message.author.id})'s message has been deleted by **${fetchedLogs.entries.first().executor.username}**#${fetchedLogs.entries.first().executor.discriminator} in ${message.channel}.`, embed,
 		);
 	}
 	else {
