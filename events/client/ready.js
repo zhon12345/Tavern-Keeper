31f5541b0df2b10d2ccbf3d91f059c54b04a1961
Ng Zhun Onn <ngzhunonn@gmail.com>
2021-01-14 16:07:30 +0000

    ✅ Added more bot status

--- events/client/ready.js
+++ events/client/ready.js
@@ -7,15 +7,21 @@ module.exports = async (client) => {
 		useUnifiedTopology: true,
 	});
 
-	const botStatus = [
+	const stats = [
 		`${client.commands.size} Commands`,
 		`${client.users.cache.size} Users`,
 		`${client.guilds.cache.size} Servers`,
 	];
 
+	const cmd = [
+		`${BOT_PREFIX}help`,
+		`${BOT_PREFIX}invite`
+	]
+
 	setInterval(function() {
-		const status = botStatus[Math.floor(Math.random() * botStatus.length)];
-		client.user.setActivity(`${BOT_PREFIX}help | ${status}`, { type: "PLAYING" });
+		const stat = stats[Math.floor(Math.random() * stats.length)];
+		const command = cmd[Math.floor(Math.random() * cmd.length)];
+		client.user.setActivity(`${BOT_PREFIX} | ${stat}`, { type: "PLAYING" });
 	}, 30000);
 
 	console.log("Connected to MongoDB");
