7f2d111107ab2e309e3a9abfcb8e7bbede285b44
zhon12345 <43035773+zhon12345@users.noreply.github.com>
2021-03-09 11:52:07 +0800

    🔧Added image format checks

--- commands/image/meme.js
+++ commands/image/meme.js
@@ -20,13 +20,20 @@ module.exports = {
 		const sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
 		const url = `https://www.reddit.com/r/${sub}/hot.json`;
 
+
 		let response;
 		try {
 			response = await fetch(url)
 				.then(res => res.json())
 				.then(json => json.data.children.map(v => v.data))
 				.then(post => {
-					const random = post[Math.floor(Math.random() * post.length) + 1];
+					let random = post[Math.floor(Math.random() * post.length) + 1];
+					while (!random || !random.url.match(/(jpg|png|gif)$/)) {
+						random = post[Math.floor(Math.random() * post.length) + 1];
+					}
+					if(random.url.endsWith("gifv")) {
+						random.url.replace("gifv", "gif");
+					}
 					const embed = new MessageEmbed()
 						.setColor("BLUE")
 						.setURL(`https://www.reddit.com/r/${random.subreddit}/comments/${random.id}`)
