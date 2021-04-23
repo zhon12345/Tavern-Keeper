f26027d8b6672980879d990c334ca946d383de94
Ng Zhun Onn <ngzhunonn@gmail.com>
2021-04-23 16:10:25 +0800

    ✅ Updated link command

--- commands/info/links.js
+++ commands/info/links.js
@@ -18,9 +18,9 @@ module.exports = {
 				{ name: "Invite link", value: "[Bot Invite](https://discord.com/oauth2/authorize?client_id=722054700308103200&scope=bot&permissions=490056959)", inline: true },
 				{ name: "Website", value: "[Bot Website](http://tavernkeeper.ml/)", inline: true },
 				{ name: "Official Discord Server", value: "[Discord Server](https://discord.gg/jMpw3jw)", inline: true },
+				{ name: "Top.gg", value: "[Vote](https://top.gg/bot/722054700308103200)", inline:true },
 				{ name: "Discord Boats", value: "[Vote](https://discord.boats/bot/722054700308103200)", inline:true },
 				{ name: "Discord Bots", value: "[Vote](https://discord.bots.gg/bots/722054700308103200)", inline:true },
-				{ name: "Top.gg", value: "[Vote](https://top.gg/bot/722054700308103200)", inline:true },
 			);
 		message.channel.send(pEmbed);
 	},
