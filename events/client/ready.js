3fdf275a4ba0e6f761a4f8ec198d154b912b0baa
Ng Zhun Onn <ngzhunonn@gmail.com>
2021-05-22 22:50:38 +0800

    🔧 Better error handling

--- events/client/ready.js
+++ events/client/ready.js
@@ -5,7 +5,14 @@ module.exports = async (client) => {
 	mongoose.connect(BOT_DB, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
-	});
+	}).then(x => {
+		console.log(
+			`Connected to MongoDB! Database: "${x.connections[0].name}"`,
+		);
+	})
+		.catch(err => {
+			console.error("Error connecting to mongo", err);
+		});
 
 	const stats = [
 		`${client.commands.size} Commands`,
@@ -24,7 +31,6 @@ module.exports = async (client) => {
 		client.user.setActivity(`${cmd} | ${stat}`, { type: "PLAYING" });
 	}, 30000);
 
-	console.log("Connected to MongoDB");
 	console.log(`Logged in as ${client.user.tag}`);
 	console.log("Prefix:", BOT_PREFIX);
 };
