5bedb603ab32b413be888cfe3859f386bf95b615
zhon12345 <43035773+zhon12345@users.noreply.github.com>
2021-06-14 10:38:14 +0800

    ✅ Readd snipe command

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -5,6 +5,17 @@ const moment = require("moment");
 
 module.exports = async (client, message) => {
 	if (message.author.bot) return;
+	const snipes = message.client.snipes.get(message.channel.id) || [];
+	snipes.unshift({
+		content: message.content,
+		author: message.author,
+		image: message.attachments.first() ? message.attachments.first().proxyURL : null,
+		date: new Date(),
+	});
+
+	snipes.splice(10);
+	message.client.snipes.set(message.channel.id, snipes);
+
 	const settings = await Guild.findOne({
 		guildID: message.guild.id,
 	});
