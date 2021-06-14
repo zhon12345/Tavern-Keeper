a6323590ace06166b7fad64f64f367bfdb34e947
zhon12345 <43035773+zhon12345@users.noreply.github.com>
2021-06-14 10:35:22 +0800

    ✏ Updated snipe command

--- src/commands/misc/snipe.js
+++ src/commands/misc/snipe.js
@@ -4,15 +4,16 @@ module.exports = {
 	name: 'snipe',
 	description: 'Shows the last deleted message from a specified channel',
 	aliases: [],
-	usage: 'snipe [number]',
+	usage: 'snipe [channel] [1-10]',
 	category: 'Misc',
 	disabled: false,
-	userperms: [],
+	userperms: ['MANAGE_MESSAGES'],
 	botperms: ['USE_EXTERNAL_EMOJIS'],
 	run: async (client, message, args) => {
 		const channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]) || message.channel;
 		const snipes = client.snipes.get(channel.id) || [];
-		const msg = snipes[args[0] || 0];
+		const num = isNaN(args[0]) || args[0] > 10 ? args[1] : args[0];
+		const msg = snipes[num || 0];
 		if (!msg) {
 			return message.channel.send(
 				`\`❌\` There is no deleted message in ${channel}`,
@@ -20,7 +21,7 @@ module.exports = {
 		}
 		const embed = new MessageEmbed()
 			.setAuthor(msg.author.tag, msg.author.displayAvatarURL({ format: 'png', dynamic: true }))
-			.setDescription(msg.content.length > 2048 ? `${msg.content.slice(0, 2045)}...` : msg.content)
+			.setDescription(`Channel: ${channel} \n\n ${msg.content.length > 2000 ? `${msg.content.slice(0, 1997)}...` : msg.content}`)
 			.setColor('BLUE')
 			.setTimestamp(msg.date);
 
