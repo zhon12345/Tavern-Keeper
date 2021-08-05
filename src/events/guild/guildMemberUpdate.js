a16a599c65f7b6c94a8c03f52aa6bb04bbc2e3e4
zhon12345 <43035773+zhon12345@users.noreply.github.com>
2021-08-05 15:04:48 +0800

    🔧 Updated guildMemberUpdate event

--- src/events/guild/guildMemberUpdate.js
+++ src/events/guild/guildMemberUpdate.js
@@ -2,16 +2,9 @@ const moment = require('moment');
 const Guild = require('../../models/guild');
 
 module.exports = async (client, oldMember, newMember) => {
-	if(!oldMember.guild.me.hasPermission('VIEW_AUDIT_LOG')) return;
 	const settings = await Guild.findOne({
 		guildID: newMember.guild.id,
 	});
-
-	const fetchedLogs = await newMember.guild.fetchAuditLogs({
-		type: 'MEMBER_UPDATE',
-	});
-	const auditLog = fetchedLogs.entries.first();
-	const { executor } = auditLog;
 
 	const logchannel = oldMember.guild.channels.cache.get(settings.settings.serverlog);
 	if (!logchannel) return;
@@ -22,7 +15,7 @@ module.exports = async (client, oldMember, newMember) => {
 	newMemberRoles.forEach(async (p) => {
 		if(!oldMemberRoles.includes(p)) {
 			logchannel.send(
-				`\`[${moment(Date.now()).format('HH:mm:ss')}]\` ✏️ **${oldMember.user.username}**#${oldMember.user.discriminator} (ID: ${oldMember.user.id}) was given the \`${p}\` role by **${executor.username}**#${executor.discriminator}.\n\`[Time]\` ${moment(newMember.createdTimestamp).format('dddd, MMMM Do YYYY, h:mm:ss a')}`,
+				`\`[${moment(Date.now()).format('HH:mm:ss')}]\` ✏️ **${oldMember.user.username}**#${oldMember.user.discriminator} (ID: ${oldMember.user.id}) was given the \`${p}\` role.\n\`[Time]\` ${moment(newMember.createdTimestamp).format('dddd, MMMM Do YYYY, h:mm:ss a')}`,
 			);
 		}
 	});
@@ -30,14 +23,23 @@ module.exports = async (client, oldMember, newMember) => {
 	oldMemberRoles.forEach(async (p) => {
 		if(!newMemberRoles.includes(p)) {
 			logchannel.send(
-				`\`[${moment(Date.now()).format('HH:mm:ss')}]\` ✏️ **${oldMember.user.username}**#${oldMember.user.discriminator} (ID: ${oldMember.user.id}) was removed from the \`${p}\` role by **${executor.username}**#${executor.discriminator}.\n\`[Time]\` ${moment(newMember.createdTimestamp).format('dddd, MMMM Do YYYY, h:mm:ss a')}`,
+				`\`[${moment(Date.now()).format('HH:mm:ss')}]\` ✏️ **${oldMember.user.username}**#${oldMember.user.discriminator} (ID: ${oldMember.user.id}) was removed from the \`${p}\` role.\n\`[Time]\` ${moment(newMember.createdTimestamp).format('dddd, MMMM Do YYYY, h:mm:ss a')}`,
 			);
 		}
 	});
 
 	if(newMember.nickname !== oldMember.nickname) {
-		logchannel.send(
-			`\`[${moment(Date.now()).format('HH:mm:ss')}]\` ✏️ **${oldMember.nickname ? oldMember.nickname : oldMember.user.username}**#${oldMember.user.discriminator} (ID: ${oldMember.user.id})'s nickname has been changed to **${newMember.nickname ? newMember.nickname : newMember.user.username}** by **${executor.username}**#${executor.discriminator}.\n\`[Time]\` ${moment(newMember.createdTimestamp).format('dddd, MMMM Do YYYY, h:mm:ss a')}`,
-		);
+		console.log(newMember.nickname);
+		if (newMember.nickname === newMember.user.username || newMember.nickname === null) {
+			logchannel.send(
+				`\`[${moment(Date.now()).format('HH:mm:ss')}]\` ✏️ **${oldMember.user.username}**#${oldMember.user.discriminator} (ID: ${oldMember.user.id})'s nickname has been removed.\n\`[Time]\` ${moment(newMember.createdTimestamp).format('dddd, MMMM Do YYYY, h:mm:ss a')}`,
+			);
+		}
+		else {
+			logchannel.send(
+				`\`[${moment(Date.now()).format('HH:mm:ss')}]\` ✏️ **${oldMember.user.username}**#${oldMember.user.discriminator} (ID: ${oldMember.user.id})'s nickname has been changed to **${newMember.nickname ? newMember.nickname : newMember.user.username}**.\n\`[Time]\` ${moment(newMember.createdTimestamp).format('dddd, MMMM Do YYYY, h:mm:ss a')}`,
+			);
+		}
 	}
+
 };
