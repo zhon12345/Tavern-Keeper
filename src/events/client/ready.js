03f1307b59cc401d8e033edeef0a02f6b7607cb4
zhon12345 <43035773+zhon12345@users.noreply.github.com>
2021-08-05 14:04:48 +0800

    🔧 Updated ready event

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -5,19 +5,32 @@ module.exports = async (client) => {
 	mongoose.connect(BOT_DB, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
-	});
+	}).then(x => {
+		console.log(
+			`Connected to MongoDB! Database: "${x.connections[0].name}"`,
+		);
+	})
+		.catch(err => {
+			console.error('Error connecting to mongo', err);
+		});
 
-	const botStatus = [
+	const stats = [
 		`${client.commands.size} Commands`,
 		`${client.users.cache.size} Users`,
 		`${client.guilds.cache.size} Servers`,
 	];
 
+	const cmds = [
+		`${BOT_PREFIX}help`,
+		`${BOT_PREFIX}invite`,
+	];
+
 	setInterval(function() {
-		const status = botStatus[Math.floor(Math.random() * botStatus.length)];
-		client.user.setActivity(`${BOT_PREFIX}help | ${status}`, { type: 'PLAYING' });
+		const stat = stats[Math.floor(Math.random() * stats.length)];
+		const cmd = cmds[Math.floor(Math.random() * cmds.length)];
+		client.user.setActivity(`${cmd} | ${stat}`, { type: 'PLAYING' });
 	}, 30000);
-	console.log('Connected to MongoDB');
+
 	console.log(`Logged in as ${client.user.tag}`);
 	console.log('Prefix:', BOT_PREFIX);
 };
