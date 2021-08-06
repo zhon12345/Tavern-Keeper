1705a7c20eb4e77ba68c223ed6e6c9b311a4a7fd
zhon12345 <43035773+zhon12345@users.noreply.github.com>
2021-08-07 00:54:52 +0800

    🔧 Fixed blacklist command

--- commands/owner/blacklist.js
+++ commands/owner/blacklist.js
@@ -12,7 +12,23 @@ module.exports = {
 	botperms: [],
 	run: async (client, message, args) => {
 		const member = message.mentions.members.first() || message.guild.members.cache.get(args[1]);
-		if(args[0] === "add") {
+		if(!args[0]) {
+			Blacklist.find({}, (err, data) => {
+				if(data && data.length > 0) {
+					const embed = new MessageEmbed()
+						.setTitle("Blacklisted Users")
+						.setColor("BLUE")
+						.setDescription(data.map(u => {return `**1** - ${client.users.cache.get(u.id)} (\`${u.name}\`)\nID: \`${u.id}\`\n`;}).join("\n"));
+					message.channel.send(embed);
+				}
+				else {
+					return message.channel.send(
+						"`⚠️` There are currently no blacklisted users.",
+					);
+				}
+			});
+		}
+		else if(args[0].toLowerCase() === "add") {
 			if (!member) {
 				return message.channel.send(
 					"`❌` User not found, please provide a valid user. (eg. `@zhon12345#8585`)",
@@ -30,11 +46,11 @@ module.exports = {
 						id : member.user.id,
 					});
 					data.save();
-					message.channel.send(`\`✔️\` \`${member.user.tag}\` has been added to blacklist.`);
+					message.channel.send(`\`✔️\` \`${member.user.tag}\` has been added to the blacklist.`);
 				}
 			});
 		}
-		else if(args[0] === "remove") {
+		else if(args[0].toLowerCase() === "remove") {
 			if (!member) {
 				return message.channel.send(
 					"`❌` User not found, please provide a valid user. (eg. `@zhon12345#8585`)",
@@ -45,7 +61,7 @@ module.exports = {
 				if(err) throw err;
 				if(data) {
 					await Blacklist.findOneAndDelete({ id : member.user.id });
-					message.channel.send(`\`✔️\` \`${member.user.tag}\` has been removed from blacklist.`);
+					message.channel.send(`\`✔️\` \`${member.user.tag}\` has been removed from the blacklist.`);
 				}
 				else {
 					message.channel.send(`\`❌\` \`${member.user.tag}\` has not been blacklisted.`);
@@ -53,20 +69,9 @@ module.exports = {
 			});
 		}
 		else {
-			Blacklist.find({}, (err, data) => {
-				if(data) {
-					const embed = new MessageEmbed()
-						.setTitle("Blacklisted Users")
-						.setColor("BLUE")
-						.setDescription(data.map(u => {return `> **Username: ${client.users.cache.get(u.id)} (\`${u.name}\`)**\n> **User ID: \`${u.id}\`**\n`;}).join("\n"));
-					message.channel.send(embed);
-				}
-				else {
-					return message.channel.send(
-						"`⚠️` There is no blacklisted member",
-					);
-				}
-			});
+			return message.channel.send(
+				"`❌` Valid argument not found, please provide valid arguments (eg. `add`).",
+			);
 		}
 	},
 };
