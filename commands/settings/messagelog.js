4b77e1d359d78ca9e89f94b1e5f9c701e004154d
zhon12345 <43035773+zhon12345@users.noreply.github.com>
2021-08-06 18:10:49 +0800

    🔧 Updated messagelog, modlog and serverlog commands

--- commands/settings/messagelog.js
+++ commands/settings/messagelog.js
@@ -3,7 +3,7 @@ const Guild = require("../../models/guild");
 module.exports = {
 	name: "messagelog",
 	category: "Settings",
-	description: "Sets the message logs channel for the server.",
+	description: "Sets the Message Log for the server.",
 	aliases: [],
 	usage: "messagelog <channel>",
 	disabled: false,
@@ -15,14 +15,26 @@ module.exports = {
 		});
 
 		const channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]);
-		if (args[0] === "off") {
-			await Guild.updateOne(
-				{ guildID:message.guild.id },
-				{ "settings.messagelog": null },
+		if (!args[0]) {
+			return message.channel.send(
+				`Message Logs for **${message.guild}** has been ${settings.settings.messagelog === null ? "`disabled`." : `set to <#${settings.settings.messagelog}>.`}`,
 			);
-			message.channel.send(
-				"`✔️` Message logs has been `disabled`",
-			);
+		}
+		else if (args[0].toLowerCase() === "off") {
+			if (settings.settings.messagelog === null) {
+				return message.channel.send(
+					"`❌` Message Logs has already been `disabled`.",
+				);
+			}
+			else {
+				await Guild.updateOne(
+					{ guildID:message.guild.id },
+					{ "settings.messagelog": null },
+				);
+				message.channel.send(
+					"`✔️` Message logs is now `disabled`.",
+				);
+			}
 		}
 		else if(channel) {
 			await Guild.updateOne(
@@ -33,14 +45,9 @@ module.exports = {
 				`\`✔️\` Message logs has been set to ${channel}`,
 			);
 		}
-		else if(settings.settings.messagelog === null) {
-			return message.channel.send(
-				`Message Logs for **${message.guild}** has been \`disabled\`.`,
-			);
-		}
 		else {
 			return message.channel.send(
-				`Message Logs for **${message.guild}** has been set to <#${settings.settings.messagelog}>.`,
+				`\`❌\` Channel not found, please provide a valid channel (eg. ${message.channel}).`,
 			);
 		}
 	},
--- commands/settings/serverlog.js
+++ commands/settings/serverlog.js
@@ -3,26 +3,38 @@ const Guild = require("../../models/guild");
 module.exports = {
 	name: "serverlog",
 	category: "Settings",
-	description: "Sets the serverlogs channel for the server.",
+	description: "Sets the Server Log for the server.",
 	aliases: [],
 	usage: "serverlog <channel>",
 	disabled: false,
 	userperms: ["ADMINISTRATOR"],
-	botperms: [],
+	botperms: ["USE_EXTERNAL_EMOJIS"],
 	run: async (client, message, args) => {
 		const settings = await Guild.findOne({
 			guildID: message.guild.id,
 		});
 
 		const channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]);
-		if (args[0] === "off") {
-			await Guild.updateOne(
-				{ guildID:message.guild.id },
-				{ "settings.serverlog": null },
+		if (!args[0]) {
+			return message.channel.send(
+				`Server Logs for **${message.guild}** has been ${settings.settings.serverlog === null ? "`disabled`." : `set to <#${settings.settings.serverlog}>.`}`,
 			);
-			message.channel.send(
-				"`✔️` Server logs has been `disabled`",
-			);
+		}
+		else if (args[0].toLowerCase() === "off") {
+			if(settings.settings.serverlog === null) {
+				return message.channel.send(
+					"`❌` Server Logs has already been `disabled`.",
+				);
+			}
+			else {
+				await Guild.updateOne(
+					{ guildID:message.guild.id },
+					{ "settings.serverlog": null },
+				);
+				message.channel.send(
+					"`✔️` Server logs is now `disabled`",
+				);
+			}
 		}
 		else if(channel) {
 			await Guild.updateOne(
@@ -33,14 +45,9 @@ module.exports = {
 				`\`✔️\` Server logs has been set to ${channel}`,
 			);
 		}
-		else if(settings.settings.serverlog === null) {
-			return message.channel.send(
-				`Server Logs for **${message.guild}** has been \`disabled\`.`,
-			);
-		}
 		else {
 			return message.channel.send(
-				`Server Logs for **${message.guild}** has been set to <#${settings.settings.serverlog}>.`,
+				`\`❌\` Channel not found, please provide a valid channel (eg. ${message.channel}).`,
 			);
 		}
 	},
