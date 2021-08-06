fbebef42f56169ccef821e98d407dc035e911519
Ng Zhun Onn <ngzhunonn@gmail.com>
2021-08-06 18:10:49 +0800

    🔧 Updated messagelog, modlog and serverlog commands

--- commands/settings/modlog.js
+++ commands/settings/modlog.js
@@ -3,7 +3,7 @@ const Guild = require("../../models/guild");
 module.exports = {
 	name: "modlog",
 	category: "Settings",
-	description: "Sets the modlogs channel for the server.",
+	description: "Sets the Mod Log for the server.",
 	aliases: [],
 	usage: "modlog <channel>",
 	disabled: false,
@@ -15,14 +15,26 @@ module.exports = {
 		});
 
 		const channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]);
-		if (args[0] === "off") {
-			await Guild.updateOne(
-				{ guildID:message.guild.id },
-				{ "settings.modlog": null },
+		if (!args[0]) {
+			return message.channel.send(
+				`Mod Logs for **${message.guild}** has been ${settings.settings.modlog === null ? "`disabled`." : `set to <#${settings.settings.modlog}>.`}`,
 			);
-			message.channel.send(
-				"`✔️` Mod logs has been `disabled`",
-			);
+		}
+		else if (args[0].toLowerCase() === "off") {
+			if (settings.settings.modlog === null) {
+				return message.channel.send(
+					"`❌` Mod Logs has already been `disabled`.",
+				);
+			}
+			else {
+				await Guild.updateOne(
+					{ guildID:message.guild.id },
+					{ "settings.modlog": null },
+				);
+				message.channel.send(
+					"`✔️` Mod Logs is now `disabled`.",
+				);
+			}
 		}
 		else if(channel) {
 			await Guild.updateOne(
@@ -30,17 +42,12 @@ module.exports = {
 				{ "settings.modlog": channel.id },
 			);
 			message.channel.send(
-				`\`✔️\` Mod logs has been set to ${channel}`,
-			);
-		}
-		else if(settings.settings.modlog === null) {
-			return message.channel.send(
-				`Mod Logs for **${message.guild}** has been \`disabled\`.`,
+				`\`✔️\` Mod Logs has been set to ${channel}`,
 			);
 		}
 		else {
 			return message.channel.send(
-				`Mod Logs for **${message.guild}** has been set to <#${settings.settings.modlog}>.`,
+				`\`❌\` Channel not found, please provide a valid channel (eg. ${message.channel}).`,
 			);
 		}
 	},
