39422127977122d0738b7224cc7b616d96e01424
zhon12345 <43035773+zhon12345@users.noreply.github.com>
2025-07-06 23:18:26 +0800

    ♻️ Replace pm2 with keepAlive script

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const { BOT_DB } = process.env;
 const { Client, GatewayIntentBits, Collection } = require("discord.js");
 const mongoose = require("mongoose");
+const keepAlive = require("./server");
 
 const client = new Client({
 	intents: [
@@ -26,9 +27,10 @@ mongoose
 	.connect(BOT_DB)
 	.then((x) => {
 		console.log(`Connected to MongoDB! Database: "${x.connections[0].name}"`);
-})
+	})
 	.catch((err) => {
 		console.error("Error connecting to mongo", err);
 	});
 
+keepAlive();
 client.login(process.env.BOT_TOKEN);
