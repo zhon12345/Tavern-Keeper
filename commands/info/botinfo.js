6c7d8e2276345b2e47013f98eb788417031f608a
zhon12345 <43035773+zhon12345@users.noreply.github.com>
2025-07-08 17:42:02 +0800

    🔥 Remove unused dependency cpu-stat & switch to node:os for system related info

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -1,13 +1,12 @@
 const { MessageEmbed, version: djsversion } = require("discord.js");
 const { formatBytes, parseDur } = require("../../functions.js");
-const cpuStat = require("cpu-stat");
 const { BOT_OWNER } = process.env;
 const moment = require("moment");
-const os = require("os");
+const os = require("node:os");
 
 const formatOS = {
 	aix: "IBM AIX",
-	darwin: "Darwin",
+	darwin: "macOS",
 	freebsd: "FreeBSD",
 	linux: "Linux",
 	openbsd: "OpenBSD",
@@ -25,33 +24,32 @@ module.exports = {
 	userperms: [],
 	botperms: ["USE_EXTERNAL_EMOJIS"],
 	run: async (client, message) => {
-		cpuStat.usagePercent(() => {
-			const embed = new MessageEmbed()
-				.setThumbnail(client.user.displayAvatarURL({ dynamic: true, size: 512 }))
-				.setColor(message.guild.members.cache.get(client.user.id).displayHexColor)
-				.setFooter(`Requested by ${message.author.tag} `)
-				.setTimestamp()
-				.setTitle("Bot Information")
-				.addField("<:documents:773950876347793449> General ❯", [
-					`> **<:card:773965449402646549> Bot Name: \`${client.user.tag}\`**`,
-					`> **\\📇 Bot ID: \`${client.user.id}\`**`,
-					`> **\\👑 Bot Owner: \`${client.users.cache.get(BOT_OWNER).tag}\`**`,
-					`> **\\🌐 Servers: \`${client.guilds.cache.size.toLocaleString()}\` Servers**`,
-					`> **\\👥 Users: \`${client.users.cache.size.toLocaleString()}\` Users**`,
-					`> **\\📺 Channels: \`${client.channels.cache.size.toLocaleString()}\` Channels**`,
-					`> **\\💬 Commands: \`${client.commands.size}\` Commands**`,
-					`> **\\📅 Created: \`${moment(client.user.createdTimestamp).format("MMMM Do YYYY, h:mm:ss")}\` | \`${Math.floor((Date.now() - client.user.createdTimestamp) / 86400000)}\` day(s) ago**`,
-					"\u200b",
-				])
-				.addField("<:documents:773950876347793449> System ❯", [
-					`> **<:online:745651877382717560> Uptime: ${parseDur(client.uptime)}**`,
-					`> **<:nodejs:773599989724348448> Node.js: \`${process.version}\`**`,
-					`> **<:djs:773599989833400371> Discord.js: \`v${djsversion}\`**`,
-					`> **\\🖥 Platform: \`${formatOS[os.platform]}\`**`,
-					`> **\\📊 Memory: \`${formatBytes(process.memoryUsage().heapUsed)} / ${formatBytes(process.memoryUsage().heapTotal)}\`**`,
-					`> **\\💻 CPU: \`${os.cpus()[0].model.trim().split("CPU")[0]} ${os.cpus().length} Cores ${os.cpus()[0].model.trim().split("CPU ")[1]}\`**`,
-				]);
-			message.channel.send(embed);
-		});
+		const embed = new MessageEmbed()
+			.setThumbnail(client.user.displayAvatarURL({ dynamic: true, size: 512 }))
+			.setColor(message.guild.members.cache.get(client.user.id).displayHexColor)
+			.setFooter(`Requested by ${message.author.tag} `)
+			.setTimestamp()
+			.setTitle("Bot Information")
+			.addField("<:documents:773950876347793449> General ❯", [
+				`> **<:card:773965449402646549> Bot Name: \`${client.user.tag}\`**`,
+				`> **\\📇 Bot ID: \`${client.user.id}\`**`,
+				`> **\\👑 Bot Owner: \`${client.users.cache.get(BOT_OWNER).tag}\`**`,
+				`> **\\🌐 Servers: \`${client.guilds.cache.size.toLocaleString()}\` Servers**`,
+				`> **\\👥 Users: \`${client.users.cache.size.toLocaleString()}\` Users**`,
+				`> **\\📺 Channels: \`${client.channels.cache.size.toLocaleString()}\` Channels**`,
+				`> **\\💬 Commands: \`${client.commands.size}\` Commands**`,
+				`> **\\📅 Created: \`${moment(client.user.createdTimestamp).format("MMMM Do YYYY, h:mm:ss")}\` | \`${Math.floor((Date.now() - client.user.createdTimestamp) / 86400000)}\` day(s) ago**`,
+				"\u200b",
+			])
+			.addField("<:documents:773950876347793449> System ❯", [
+				`> **<:online:745651877382717560> Uptime: ${parseDur(client.uptime)}**`,
+				`> **<:nodejs:773599989724348448> Node.js: \`${process.version}\`**`,
+				`> **<:djs:773599989833400371> Discord.js: \`v${djsversion}\`**`,
+				`> **\\🖥 Platform: \`${formatOS[os.platform]}\`**`,
+				`> **\\📊 Memory: \`${formatBytes(process.memoryUsage().heapUsed)} / ${formatBytes(process.memoryUsage().heapTotal)}\`**`,
+				`> **\\💻 CPU: \`${os.cpus()[0].model.trim().split("CPU")[0]} ${os.cpus().length} Cores\`**`,
+			]);
+
+		message.channel.send(embed);
 	},
 };
